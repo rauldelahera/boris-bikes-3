2cd1a41a849ffc0cb0a4af91aee0f0b851b84026
Raul <rauldelahera@hotmail.com>
2021-10-13 16:13:36 +0100

    Chapter 11, in progress

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,18 @@
 class DockingStation
+  attr_reader :bikes_in_dock
+
+  def initialize
+    @bikes_in_dock = []
+  end
+
   def release_bike
-    return Bike.new
+    bike = Bike.new
+  end
+
+  def dock(bike)
+    # dock = DockingStation.new
+    @bikes_in_dock << bike
+    return @bikes_in_dock[0]
   end
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,17 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it 'docking_station should release bike' do
     expect(subject).to respond_to :release_bike
   end
+
   it 'release_bike should return a bike object' do
     expect(subject.release_bike).to be_instance_of Bike
+  end
+
+  it 'docks a bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
   end
 end
