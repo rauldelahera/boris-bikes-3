e951b4cf8743373545de3ed5f7e842463004e9e4
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-10-14 14:14:18 +0100

    challenge 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,17 +3,17 @@ require 'bike'
 class DockingStation
   attr_reader :bike
 
-  def release_bike
+  def release_bike  #creating an instance of the Bike class
     bike = Bike.new
     # @bike = bike
   end
 
-  def dock(bike)
+  def dock(bike) #docking a working bike
     @bike = bike
   end
 
-  # def bike
-  #   @bike
-  # end
+  def bike 
+    @bike
+  end
 end
 
